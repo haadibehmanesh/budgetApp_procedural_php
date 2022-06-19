01cb26fbc1f3a380ad070754ef97dda3ccb7e811
Hadi Behmanesh <haadibehmanesh@gmail.com>
2022-06-19 18:49:23 +0200

    add extractTransaction to replace $ and , from amount for each transaction

--- app/App.php
+++ app/App.php
@@ -39,3 +39,18 @@ function getTransactions(string $fileName, ?callable $transactionHandler = null)
 
     return $transactions;
 }
+
+
+function extractTransaction(array $transactionRow): array
+{
+    [$date, $checkNumber, $description, $amount] = $transactionRow;
+
+    $amount = (float) str_replace(['$', ','], '', $amount);
+
+    return [
+        'date'        => $date,
+        'checkNumber' => $checkNumber,
+        'description' => $description,
+        'amount'      => $amount,
+    ];
+}
