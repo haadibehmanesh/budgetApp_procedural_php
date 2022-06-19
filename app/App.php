d6c6c6056f58160a43f3d129d967e92ddb360abb
Hadi Behmanesh <haadibehmanesh@gmail.com>
2022-06-19 18:25:42 +0200

    make getTransactionFiles function to get transaction files

--- app/App.php
+++ app/App.php
@@ -1,3 +1,18 @@
 <?php
 
 declare(strict_types = 1);
+
+function getTransactionFiles(string $dirPath): array
+{
+    $files = [];
+
+    foreach (scandir($dirPath) as $file) {
+        if (is_dir($file)) {
+            continue;
+        }
+
+        $files[] = $dirPath . $file;
+    }
+
+    return $files;
+}
